98ceaee7e77efa0f84d3e36ef0685963a65ba4a7
Adrian Toth <t.adrian88@gmail.com>
2020-09-26 13:08:01 +0100

    Add test for fizz,buzz,fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,2 +1,11 @@
-def fizzbuzz
+def fizzbuzz(num)
+  if num % 15 == 0
+    return "fizzbuzz"
+  elsif num % 5 == 0
+    return "buzz"
+  elsif num % 3 == 0
+    return "fizz"
+  else
+    return num
+  end
 end
